eb934edcde05efbcedc1b8724d98a5d1b1de73e1
Sussex123456143 <archithsaisurya@gmail.com>
2024-11-07 23:10:15 -0800

    Example uses

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "LineSlope.h"
 int main(){
-    std::cout << findY(0.25,0.25,1,1,-0.5) << std::endl;
+    //std::cout << findY(0.25,0.25,1,1,-0.5) << std::endl;
+    //LineSlope line = LineSlope(3.1,6.3,4.7,12.24);
+    //std::cout << line.calcX(3.69)<< " " << line.calcY(4.768)<< std::endl;
     return 0;
 }
