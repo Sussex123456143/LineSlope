d4514c7d8a0013f7efbb048a394e25a2a2984b0a
Sussex123456143 <archithsaisurya@gmail.com>
2024-11-07 23:06:38 -0800

    Added LineSlope class with special functions

--- LineSlope.h
+++ LineSlope.h
@@ -15,4 +15,40 @@ float findY(float x1, float y1, float x2, float y2, float x){
     float b = calcB(x1, y1, m);
     return calcY(x,m,b);
 }
+class LineSlope{
+private:
+    float m,b;
+public:
+    LineSlope(float x1, float y1, float x2, float y2){
+        m = calcM(x1,y1,x2,y2);
+        b = calcB(x1,y1,m);
+    }
+    LineSlope(float m, float b){
+        this -> m = m;
+        this -> b = b;
+    }
+    float calcX(float x){
+        return m*x + b;
+    }
+    float calcY(float y){
+        return (y-b)/m;
+    }
+    bool contains(float x,float y){
+        if (y == m*x + b) return true;
+        else return false;
+    }
+    float getM(){
+        return m;
+    }
+    float setM(float m){
+        this -> m = m;
+    }
+    float getB(){
+        return b;
+    }
+    float setB(float b){
+        this -> b = b;
+    }
+
+};
 #endif
